59f1294def4b4565cd70f0927a6bac05dfffe0a6
duanshize <874492787@qq.com>
2019-01-11 18:34:27 +0800

    项目基本环境搭建完成

---
+++ Manage_CreditManagement/Manage_Controller/src/main/java/controller/PasswordController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import Impl.TestImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PasswordController
+{
+
+    @RequestMapping("/changePassword")
+    public String changePassword(String id,String oldPassword,String newPassword )
+    {
+
+        return "hello";
+    }
+}
